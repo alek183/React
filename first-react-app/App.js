2704473681d02bffe375e5a5a81c6d0defc64011
Alek Novakovic <alek.novakovic18@gmail.com>
2026-02-18 20:09:54 +0100

    Module 7

--- first-react-app/App.js
+++ first-react-app/App.js
@@ -8,14 +8,19 @@ import TestScreen from './screens/TestScreen';
 import ListScreen from './screens/ListScreen';
 import ButtonScreen from './screens/ButtonScreen';
 import PersonalScreen from './screens/PersonalScreen';
+import MenuScreen from './screens/MenuScreen';
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name = "Home" component = {PersonalScreen}/>
+      <Stack.Navigator initialRouteName = "Menu">
+        <Stack.Screen name = "Home" component = {MainScreen}/>
+        <Stack.Screen name = "Button" component = {ButtonScreen}/>
+        <Stack.Screen name = "List" component = {ListScreen}/>
+        <Stack.Screen name = "Exercise" component = {ExerciseScreen}/>
+        <Stack.Screen name = "Menu" component = {MenuScreen}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
