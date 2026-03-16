9a48780d2b328c731c0ea9117a9b6ddbc714617b
Alek Novakovic <alek.novakovic18@gmail.com>
2026-03-16 21:16:12 +0100

    Module 17

---
+++ dgschool-alek-novakovic-rn/src/screens/Contact.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const Contact = ({ params }) => (
+    <View>
+        <Text>Contact Page</Text>
+    </View>
+);
+
+export default Contact;
--- dgschool-alek-novakovic-rn/src/screens/Home.js
+++ dgschool-alek-novakovic-rn/src/screens/Home.js
@@ -4,6 +4,7 @@ import { Text, View, StyleSheet } from 'react-native';
 const Home = ({ params }) => (
     <View>
         <Text>Welcome to Home Screen</Text>
+         <Button title = "Go to About" onPress={() => navigation.navigate("About")}/>
     </View>
 );
 
